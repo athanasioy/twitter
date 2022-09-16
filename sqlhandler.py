c4417d2ed1539e82f302e611136302610c22a4ec
athanasioy <athanasioy.antonis@gmail.com>
2022-09-16 18:19:32 +0300

    Introducted field_mapping to the sqlHandler class. Mainly responsible for mapping dataclasses fields to database fields.
    Seperated also the responsibility of building insert parameters to 
    insert_parameter_builder method of sqlHandler class

--- sqlhandler.py
+++ sqlhandler.py
@@ -9,26 +9,41 @@ class sqlHandler:
         self.connection = pyodbc.connect(conn_string) # Creates a connection with DB server
         self.cursor = self.connection.cursor()
 
-    def insert_users(self, author: Author):
+    def insert_users(self, author: Author) -> None:
         """
         Inserts a user into the authors table.
         """
         #something like ...
         self.cursor.execute("INSERT INTO Authors () values (?,?...)",params)
         pass
-    def insert_tweets(self, tweet: Tweet):
+
+    def set_field_mapper(self,map_dict: dict) -> None:
+        """Sets the field mapper dict into a class variable"""
+        self.field_mapping = map_dict
+
+    def insert_parameter_builder(self, tweet: Tweet) -> str:
+        fields_to_insert = '('
+        for field in fields(tweet):
+            fields_to_insert += self.field_mapper("Tweets",field.name) + ','
+        fields_to_insert = fields_to_insert[:-1]+')'
+        return fields_to_insert
+
+    def insert_tweets(self, tweet: Tweet) -> None:
         """
         Inserts a tweet into the Tweets Table.
         Before inserting, it checks if the author exists into the authors table
         """
-        fields_to_insert = '('
-        for field in fields(tweet):
-            fields_to_insert += field.name + ','
-        fields_to_insert = fields_to_insert[:-1]+')'
+        fields_to_insert = self.insert_parameter_builder(tweet)
         print(fields_to_insert)
-        # sql_text = "INSERT INTO Tweets (id)"
 
-    def check_if_author_exists(self, author: Author):
+
+    def field_mapper(self, table_name:str, field_name: str) -> str:
+        """Gets as input the field name from the python dataclass
+        and returns the field corresponding to the database table"""
+
+        return self.field_mapping.get(table_name).get(field_name)
+
+    def check_if_author_exists(self, author: Author) -> bool:
         """
         Returns None if author doesn't exist
         """
@@ -42,6 +57,15 @@ class sqlHandler:
 def main():
     conn_string = 'DRIVER={ODBC Driver 17 for SQL Server};SERVER=ATHANANTONIS;DATABASE=Tweeterdb;Trusted_connection=yes'
     sql_handler = sqlHandler(conn_string)
+    field_mapping = {"Tweets":{"id": "id",
+                                "author_id": "author_id",
+                                "created_at": "created_at",
+                                "text": "tweet_text",
+                                "retweet_id": "referenced_tweet",
+                                "reference_type": "reference_type",
+                                "reply_to": "AnswersTo",
+                                "source": "source"}}
+    sql_handler.set_field_mapper(field_mapping)
 
     test_tweet = Tweet(id = -1,
                        author_id = 999,
